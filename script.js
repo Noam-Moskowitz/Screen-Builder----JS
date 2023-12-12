071e070866a82ca311121b248b3837885f09658e
Noam Moskowitz <noammz101@gmail.com>
2023-12-12 15:15:23 +0200

    added more fields, delete export and clear buttons

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 const screen = document.getElementById('screen');
 const designForm = document.getElementById('designForm');
-const submitBtn = document.getElementById('submit')
+const submitBtn = document.getElementById('submit');
+const deleteBtn = document.getElementById('delete');
+const elementInput = document.getElementById('elementType');
 let selected = screen
+
+elementInput.addEventListener('keyup', (event) => {
+    const source = document.getElementById('source');
+    if (elementInput.value == 'a' || elementInput.value == 'img') {
+        source.classList.add('input-container');
+        source.classList.remove('displayNone');
+    } else {
+        source.classList.add('displayNone');
+        source.classList.remove('input-container');
+    }
+})
 
 screen.addEventListener("click", (event) => {
     selected.classList.remove('selected')
     selected = event.target;
     selected.classList.add('selected')
-    console.log(selected);
 })
+
+deleteBtn.addEventListener("click", () => {
+    if (selected !== screen) {
+        selected.remove();
+        selected = screen;
+    }
+});
 submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
     const element = {
-        elementType: event.target.form['elementType'].value,
+        elementType: event.target.form['elementType'].value.trim(),
         width: event.target.form['width'].value,
         height: event.target.form['height'].value,
         bgc: event.target.form['backgroundColor'].value,
+        src: event.target.form['src'].value,
         content: event.target.form['content'].value,
         fColor: event.target.form['fColor'].value,
         fSize: event.target.form['fSize'].value,
+        textAlign: event.target.form['textAlign'].value,
+        textDecoration: event.target.form['textDecoration'].value,
         fFamily: event.target.form['fFamily'].value,
         position: event.target.form['Position'].value,
         top: event.target.form['Top'].value,
@@ -36,12 +58,25 @@ submitBtn.addEventListener("click", (event) => {
         borderStyle: event.target.form['borderStyle'].value,
     }
     let item = document.createElement(element.elementType)
+    if (element.width.indexOf('vw') !== -1) {
+        let numValue = element.width.replace(/\D/g, "");
+        element.width = numValue + '%';
+    }
+    if (element.height.indexOf('vh') !== -1) {
+        let numValue = element.height.replace(/\D/g, "");
+        element.height = numValue + '%';
+    }
+
     item.style.width = element.width;
     item.style.height = element.height;
     item.style.backgroundColor = element.bgc;
+    item.href = element.src;
+    item.src = element.src;
     item.innerHTML = element.content;
     item.style.color = element.fColor;
     item.style.fontSize = element.fSize;
+    item.style.textAlign = element.textAlign;
+    item.style.textDecoration = element.textDecoration;
     item.style.fontFamily = element.fFamily;
     item.style.position = element.position;
     item.style.top = element.top;
@@ -55,7 +90,12 @@ submitBtn.addEventListener("click", (event) => {
     item.style.border = `${element.borderSize} ${element.borderStyle} ${element.borderColor}`;
     item.style.borderRadius = element.borderRadius;
     selected.appendChild(item);
+    console.log(`
 
-    selected=screen;
+    ${item.src}
+    `);
+
+    selected.classList.remove('selected')
+    selected = screen;
     event.target.form.reset();
 })
